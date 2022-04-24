80e92e706bb253ae65222db046b6dd2e2133d227
Kaio Bruno Barbosa Magalhães <bmkaiobruno@gmail.com>
2022-04-24 14:43:37 -0300

    change language

---
+++ js/language.js
@@ -0,0 +1,29 @@
+$(function() {
+    $(".en").click(function() { 
+
+    $(".navbar").children ().eq (0).text ("About me");
+    $(".navbar").children ().eq (1).text ("Resume");
+    $(".navbar").children ().eq (2).text("Projects");
+    $(".navbar").children ().eq (3).text ("Contact me");
+    $(".language-selected").removeClass("change-en");
+    $(".language-selected").removeClass("change-br");
+    $(".language-selected").addClass(".change-en");
+
+    $(".home-title").text("Front end Developer and designer");
+    });
+});
+
+$(function() {
+    $(".br").click(function() { 
+
+    $(".navbar").children ().eq (0).text ("Sobre mim");
+    $(".navbar").children ().eq (1).text ("Curriculo");
+    $(".navbar").children ().eq (2).text("Projetos");
+    $(".navbar").children ().eq (3).text ("Contate-me");
+    $(".language-selected").removeClass("change-en");
+    $(".language-selected").removeClass("change-br");
+    $(".language-selected").addClass(".change-br");
+
+    $(".home-title").text("Front end Desenvolvedor e designer");
+    });
+});
